b78f5c5cf59b5eaa9ce7cbd2401fdafa5073cccd
emili <emiliotoli83@gmail.com>
2022-10-26 13:24:26 +0200

    connessione database mainactivity

--- app/src/main/java/it/uninsubria/myaudio/MainActivity.kt
+++ app/src/main/java/it/uninsubria/myaudio/MainActivity.kt
@@ -2,10 +2,21 @@ package it.uninsubria.myaudio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var db : AppDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //connessione con il database
+        db = Room.databaseBuilder(
+            this,
+            AppDatabase::class.java,
+            "audioRecords"
+        ).build()
     }
 }

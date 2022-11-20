744a89d4a31aa4b3459bb61af4aa166440665612
Alessandro Macrina <alessandro.macrina1997@gmail.com>
2022-11-20 18:27:36 +0100

    iniziata implementazione del recorder. Richiesta permesso a registrare

--- app/src/main/java/it/uninsubria/myaudio/MainActivity.kt
+++ app/src/main/java/it/uninsubria/myaudio/MainActivity.kt
@@ -1,11 +1,50 @@
 package it.uninsubria.myaudio
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+const val REQUEST_CODE = 200
 
 class MainActivity : AppCompatActivity() {
+    private var permissions = arrayOf(Manifest.permission.RECORD_AUDIO)
+    private var permissionGranted = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //verifico se il permesso è stato dato
+        permissionGranted = ActivityCompat.checkSelfPermission(this, permissions[0]) == PackageManager.PERMISSION_GRANTED
+        //se non ho i permessi li chiedo
+        if(!permissionGranted)
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
+
+        btn_record.setOnClickListener{
+            startRecording()
+        }
+    }
+
+    //metodo per chiedere il permesso, se viene dato modifico permissionGranted a true
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE)
+            permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+    }
+
+    //metodo per registrare
+    private fun startRecording(){
+        //controllo se ho i permessi per registrare
+        if(!permissionGranted){
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
+            return
+        }
+
     }
 }

a5cf44e8d8e47e28c250af7e094b18adbe161d6d
Alessandro Macrina <alessandro.macrina1997@gmail.com>
2022-11-20 18:55:00 +0100

    implementati metodi per registrare audio

--- app/src/main/java/it/uninsubria/myaudio/MainActivity.kt
+++ app/src/main/java/it/uninsubria/myaudio/MainActivity.kt
@@ -2,16 +2,29 @@ package it.uninsubria.myaudio
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.media.MediaRecorder
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 const val REQUEST_CODE = 200
 
 class MainActivity : AppCompatActivity() {
     private var permissions = arrayOf(Manifest.permission.RECORD_AUDIO)
     private var permissionGranted = false
+
+    private lateinit var recorder: MediaRecorder
+    private var dirPath = ""
+    private var fileName = ""
+    private var isRecording = false
+    private var isPaused = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,7 +36,11 @@ class MainActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
 
         btn_record.setOnClickListener{
-            startRecording()
+            when{
+                isPaused -> resumeRecorder()
+                isRecording -> pauseRecorder()
+                else -> startRecording()
+            }
         }
     }
 
@@ -38,6 +55,19 @@ class MainActivity : AppCompatActivity() {
             permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED
     }
 
+
+    private fun pauseRecorder(){
+        recorder.pause()
+        isPaused = true
+        btn_record.setImageResource(R.drawable.ic_record)
+    }
+
+    private fun resumeRecorder(){
+        recorder.pause()
+        isPaused = false
+        btn_record.setImageResource(R.drawable.ic_pause)
+    }
+
     //metodo per registrare
     private fun startRecording(){
         //controllo se ho i permessi per registrare
@@ -45,6 +75,28 @@ class MainActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
             return
         }
+        //inizio a registrare
+        recorder = MediaRecorder()
+        dirPath = "${externalCacheDir?.absolutePath}/"
+        var simpleDateFormat = SimpleDateFormat("DD.MM.yy_hh.mm.ss")
+        var date = simpleDateFormat.format(Date())
+        fileName = "audio_record_$date"
 
+        recorder.apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+            setOutputFile("$dirPath$fileName.mp3")
+
+            try {
+                prepare()
+            }catch (e: IOException){}
+
+            start()
+        }
+
+        btn_record.setImageResource(R.drawable.ic_pause)
+        isRecording = true
+        isPaused = false
     }
 }

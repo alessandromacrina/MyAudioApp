b0fa61502b1848650b6fc57c07d4a1d93841411c
emili <emiliotoli83@gmail.com>
2022-12-16 16:44:09 +0100

    collegamento button play e seek bar nella classe. creazione metodo play/pause per button play

--- app/src/main/java/it/uninsubria/myaudio/PlayerActivity.kt
+++ app/src/main/java/it/uninsubria/myaudio/PlayerActivity.kt
@@ -1,11 +1,52 @@
 package it.uninsubria.myaudio
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.SeekBar
+import androidx.core.content.res.ResourcesCompat
 
 class PlayerActivity : AppCompatActivity() {
+
+    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var btnPlay : ImageButton
+    private lateinit var seekBar : SeekBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.player_activity)
+
+        var filepath = intent.getStringExtra("filepath")
+        var filename = intent.getStringExtra("filename")
+
+        mediaPlayer = MediaPlayer()
+        mediaPlayer.apply {
+            setDataSource("filepath")
+            prepare()
+        }
+        btnPlay = findViewById(R.id.btn_play)
+        seekBar = findViewById(R.id.seekBar_play)
+
+        btnPlay.setOnClickListener{
+
+
+
+        }
+
+
+
+
+    }
+    private fun playPause(){
+        if(!mediaPlayer.isPlaying){
+            mediaPlayer.start()
+            btnPlay.background = ResourcesCompat.getDrawable(resources , R.drawable.ic_round_pause_circle_filled_24 , theme)
+
+        }
+        else
+        {
+            mediaPlayer.pause()
+            btnPlay.background = ResourcesCompat.getDrawable(resources , R.drawable.ic_round_play_circle_filled_24 , theme)
+        }
     }
 }

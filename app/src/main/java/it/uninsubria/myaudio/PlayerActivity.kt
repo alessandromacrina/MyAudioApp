231e6fe7d0f9e5759eed8c74d0c630624b4e10bd
emili <emiliotoli83@gmail.com>
2022-12-30 18:39:53 +0100

    textview con nomeapp e titolo audio

--- app/src/main/java/it/uninsubria/myaudio/PlayerActivity.kt
+++ app/src/main/java/it/uninsubria/myaudio/PlayerActivity.kt
@@ -9,7 +9,7 @@ import android.widget.ImageButton
 import android.widget.SeekBar
 import android.widget.TextView
 import androidx.core.content.res.ResourcesCompat
-import kotlinx.android.synthetic.main.player_activity.*
+import it.uninsubria.myaudio.R.*
 import java.text.DecimalFormat
 import java.text.NumberFormat
 
@@ -18,7 +18,7 @@ class PlayerActivity : AppCompatActivity() {
     private lateinit var mediaPlayer: MediaPlayer
     private lateinit var btnPlay: ImageButton
     private lateinit var seekBar: SeekBar
-
+    private lateinit var textViewFileName: TextView
     private lateinit var runnable: Runnable
     private lateinit var handler: Handler
     private var delay = 0L
@@ -28,23 +28,24 @@ class PlayerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.player_activity)
-        btnPlay = findViewById(R.id.btn_play)
+        setContentView(layout.player_activity)
+        btnPlay = findViewById(id.btn_play)
         var filepath = intent.getStringExtra("filepath")
         var filename = intent.getStringExtra("filename")
 
 
 
         mediaPlayer = MediaPlayer()
-        seekBar = findViewById(R.id.seekBar_play)
-        tvTimeLeft= findViewById(R.id.tv_timeleft)
-        tvTimeRight=findViewById(R.id.tv_timeright)
+        seekBar = findViewById(id.seekBar_play)
+        textViewFileName = findViewById(id.tv_nameFile1)
+        tvTimeLeft= findViewById(id.tv_timeleft)
+        tvTimeRight=findViewById(id.tv_timeright)
         mediaPlayer.apply {
             setDataSource(filepath)
             prepare()
         }
         tvTimeRight.text= durationFormat(mediaPlayer.duration)
-
+        textViewFileName.text=filename
         btnPlay.setOnClickListener{
             playPause()
         }
@@ -66,7 +67,7 @@ class PlayerActivity : AppCompatActivity() {
     private fun playPause(){
         if(!mediaPlayer.isPlaying){
             mediaPlayer.start()
-            btnPlay.background = ResourcesCompat.getDrawable(resources , R.drawable.ic_round_pause_circle_filled_24 , theme)
+            btnPlay.background = ResourcesCompat.getDrawable(resources , drawable.ic_round_pause_circle_filled_24 , theme)
             handler.postDelayed(runnable,delay) //chiama la funzione in loop ogni secondo
 
         }
@@ -98,7 +99,7 @@ class PlayerActivity : AppCompatActivity() {
     }
 
     private fun stop(){
-        btnPlay.background = ResourcesCompat.getDrawable(resources , R.drawable.ic_round_play_circle_filled_24 , theme)
+        btnPlay.background = ResourcesCompat.getDrawable(resources , drawable.ic_round_play_circle_filled_24 , theme)
         handler.removeCallbacks(runnable) //rimuove il runnable dalla lista degli oggetti per il loop
     }
 
